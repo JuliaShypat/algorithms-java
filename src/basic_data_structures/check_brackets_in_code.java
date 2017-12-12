4f25f490ee4961abb511584f59395fc146cfcb81
Julia Shypat <juliashipat@gmail.com>
2017-12-12 21:33:24 +0100

    Basic Data Sructures - Check Bracket[DONE]

--- src/basic_data_structures/check_brackets_in_code.java
+++ src/basic_data_structures/check_brackets_in_code.java
@@ -5,6 +5,19 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Stack;
 
+/*
+ * Matching closing brackets
+ * 	Input:
+	[]
+	Output:
+	Success
+ * 
+ * 	Input:
+	foo(bar[i);
+	Output:
+	10
+ * */
+
 class Bracket {
     Bracket(char type, int position) {
         this.type = type;
@@ -24,44 +37,44 @@ class Bracket {
     char type;
     int position;
 }
+class StackofBrackets{ 
+    static Stack<Bracket> o_brackets_stack = new Stack<Bracket>();
+    static int wrongPosition;
+}
 
 class check_brackets_in_code {
     public static void main(String[] args) throws IOException {
         InputStreamReader input_stream = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input_stream);
         String text = reader.readLine();
-        int wrongIndex = 0;
-        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        if(IsBalanced(text)) 
+        	System.out.println("Success");
+        else 
+        	System.out.println(StackofBrackets.wrongPosition);
+    }
+    
+    public static boolean IsBalanced(String text){
         for (int position = 0; position < text.length(); ++position) {
             char next = text.charAt(position);
             if (next == '(' || next == '[' || next == '{') {
-            	opening_brackets_stack.push(new Bracket(next, position));
+            	StackofBrackets.o_brackets_stack.push(new Bracket(next, position));
             	if(position + 1 == text.length()) {
-            		wrongIndex = position+1;
+            		StackofBrackets.wrongPosition = position + 1;
             	}
             }
-
-            if (next == ')' || next == ']' || next == '}') {
-            	if (opening_brackets_stack.empty()) {
-            		wrongIndex = position+1;
-            		break;
+            else if (next == ')' || next == ']' || next == '}') {
+            	if (StackofBrackets.o_brackets_stack.empty()) {
+            		StackofBrackets.wrongPosition = position + 1;
+            		return false;
             	}
-            	Bracket top = opening_brackets_stack.pop();
+            	Bracket top = StackofBrackets.o_brackets_stack.pop();
+        		StackofBrackets.wrongPosition = top.position;
             	if (!top.Match(next)) {
-            		wrongIndex = position + 1;
-            		break;
+            		StackofBrackets.wrongPosition = position + 1;
+            		return false;
             	} 
-            }
+            } 
         }
-        
-        if (wrongIndex > 0 || !opening_brackets_stack.empty()) {
-        	System.out.println(wrongIndex);
-        } else {
-        	System.out.println("Success");
-        }
-    }
-    
-    public boolean IsBalanced(String text){
-    	return false;
+       return StackofBrackets.o_brackets_stack.empty();
     }
 }
