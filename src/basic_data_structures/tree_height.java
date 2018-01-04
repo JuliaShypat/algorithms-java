622faed66428c26de3e0d55b6d6f78c889a4d0c0
Yuliia Shypat <juliashipat@gmail.com>
2018-01-04 17:05:21 +0100

    Working version of tree height

--- src/basic_data_structures/tree_height.java
+++ src/basic_data_structures/tree_height.java
@@ -36,13 +36,25 @@ public class tree_height {
 		}
 
 		int computeHeight() {
-            // Replace this code with a faster implementation
 			int maxHeight = 0;
+			int[] heights = new int[parent.length];
 			for (int vertex = 0; vertex < n; vertex++) {
+				if (heights[vertex] != 0)
+					continue;
 				int height = 0;
-				for (int i = vertex; i != -1; i = parent[i])
+				for (int i = vertex; i != -1; i = parent[i]) {
+					if (heights[i] != 0) {
+						height += heights[i];   
+						break;
+					}
 					height++;
+				}
 				maxHeight = Math.max(maxHeight, height);
+				for (int i = vertex; i != -1; i = parent[i]) {
+					if (heights[i] != 0)
+						break;
+					heights[i] = height--;
+				}
 			}
 			return maxHeight;
 		}
